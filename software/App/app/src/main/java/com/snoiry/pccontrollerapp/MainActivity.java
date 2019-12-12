b672fad9812ce4e004b432fae8f1350b5b1fad08
sylvain <sylvain.noiry@hotmail.fr>
2019-12-12 22:52:42 +0100

    feat : add buttons on App
    - add power and reset button on android App

--- software/App/app/src/main/java/com/snoiry/pccontrollerapp/MainActivity.java
+++ software/App/app/src/main/java/com/snoiry/pccontrollerapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.snoiry.pccontrollerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton power_button;
+    private ImageButton reset_button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.power_button = (ImageButton) findViewById(R.id.power_button);
+        this.reset_button = (ImageButton) findViewById(R.id.reset_button);
+
+        power_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        reset_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
