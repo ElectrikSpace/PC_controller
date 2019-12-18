8eee757b561b8b2507ec491c6ff9047949afeace
sylvain <sylvain.noiry@hotmail.fr>
2019-12-18 22:42:19 +0100

    AVR : support of 2 way HC12

--- software/PC_controller_AVR/PC_controler_AVR/main.c
+++ software/PC_controller_AVR/PC_controler_AVR/main.c
@@ -22,9 +22,19 @@ int timeout = 0;
 uint8_t message[total_bytes_message] = {0};
 uint8_t action;
 
+ISR(TIM0_OVF_vect)
+{
+	timeout+=1;
+}
+
+ISR(INT0_vect)
+{
+	sleep_disable();
+}
+
 void init() {
-	PORTB = 0x00; // outputs set to LOW and input set to h-Z
-	DDRB = 0b00011010; // set inputs and outputs
+	PORTB = 0b0000100; // outputs set to LOW and input set to h-Z
+	DDRB = 0b00011100; // set inputs and outputs
 	ADMUX = 0x00; // ADC multiplexer timer 
 	ADCSRA = 0x87; // initialize ADC status register
 	DIDR0 = 0x01;
@@ -33,8 +43,8 @@ void init() {
 
 void go_to_sleep() {
 	ADCSRA &= ~(1<<ADEN);
-	WDTCR=(0<<WDTIF) | (0<<WDTIE) | (1<<WDP3) | (1<<WDCE) | (0<<WDE) | (0<<WDP2) | (0<<WDP1) | (1<<WDP0);
-	WDTCR=(1<<WDTIF) | (1<<WDTIE) | (1<<WDP3) | (0<<WDCE) | (0<<WDE) | (0<<WDP2) | (0<<WDP1) | (1<<WDP0);
+	MCUCR &= ~(_BV(ISC01) | _BV(ISC00));      //INT0 on low level
+	GIMSK |= _BV(INT0);                       //enable INT0
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	sleep_enable();
 	sei();
@@ -43,52 +53,66 @@ void go_to_sleep() {
 	ADCSRA |= (1<<ADEN);
 }
 
-ISR(WDT_vect)
-{
-	sleep_disable();
-}
-
-char check_battery() {
+void read_battery_level() {
 	ADCSRA |= (1 << ADSC); // start conversion
 	while(ADCSRA & (1 << ADSC)); // wait until complete
-	return('y');
-	/*if(ADC >= min_batterie_level) {
-		return('y');		
-	}
-	else{
-		return('n');
-	}*/
+	return;
 }
 
-ISR(TIM0_OVF_vect)
-{
-	 timeout+=1;
+void HC12_write(uint8_t data) { // send a bytes other HC12
+	PORTB = 0b00000000;
+	for (int i = 7; i >= 0; i--) {
+		_delay_us(200);
+		if ( (data & (1<<i)) == 0x00 ){
+			PORTB = 0b00000000;
+		}
+		else {
+			PORTB = 0b00000100;
+		}
+	}
+	_delay_us(200);
+	PORTB = 0b00000100;
+	_delay_us(200);
 }
 
 void HC12_read(){
 	timeout = 0;
-	int remain_bytes = total_bytes_message;
-	while((timeout<=2000) & (remain_bytes > 0)){
-		if( !(PINB & (1<<2)) ) { // start bit
-			uint8_t value = 0x00;
-			_delay_us(140); // one and half a period
-			for(int index = 0; index <= 7; index++){ // values
-				if(PINB & (1<<2)){
+	TCCR0B |= _BV(CS02)|_BV(CS00); // set prescaler to 1024 (CLK=1200000Hz/1024/256=4Hz, 0.25s)
+	TIMSK0 |= _BV(TOIE0); // enable Timer Overflow interrupt
+	sei(); // enable global interrupts
+	int remain_bytes = 0;
+	uint8_t value = 0x00;
+	while((timeout<=3) & (remain_bytes > 0)){
+		if( !(PINB & (1<<1)) ) { // start bit
+			_delay_us(290); // one and half a period
+			for(int index = 0; index <= 7; index++) { // values
+				if(PINB & (1<<1)){
 					value = value | (1<<index);
 				}
-				_delay_us(90); // next bit 9600 HZ
+				_delay_us(190); // next bit 4800 HZ
 			}
-			if(PINB & (1<<2)){ // stop bit
-				remain_bytes--;
-				if(remain_bytes == 0) {
+			if(PINB & (1<<1)){ // stop bit
+				if(remain_bytes != 0) {
 					message[total_bytes_message - remain_bytes] = value;
+					value = 0x00;
+					remain_bytes--;
 				}
 				else {
-					action = value;
+					if (action == 0x00) {
+						action = value;
+						value = 0x00;
+						if (action == 0x03){
+							return; // this is a battery level request
+						}
+						else {
+							remain_bytes = total_bytes_message;
+						}
+					}
+					else {
+						value = 0x00;
+						return;
+					}
 				}
-				value = 0x00;
-				
-				timeout = 2500;
 			}
 			else{
 				// error
@@ -102,57 +126,48 @@ int main(void)
     init();
     while (1) 
     {
-		if(check_battery() == 'y') {
-			TCCR0B |= _BV(CS02)|_BV(CS00); // set prescaler to 1024 (CLK=1200000Hz/1024/256=4Hz, 0.25s)
-			TIMSK0 |= _BV(TOIE0); // enable Timer Overflow interrupt
-			sei(); // enable global interrupts
-			while(counter<=50000){
-				PORTB = 0x02; //enable HC12
-				_delay_ms(1);
-				timeout = 0;
-				while(timeout <= 1){
-					if( !(PINB & (1<<2)) ) {
-						_delay_ms(250); // wait until before new complete message
-						HC12_read(); 
-						/*if (message == key) {
-							if (action == 0x01) {*/
-								PORTB = 0b00001000; // PB3 ON = power switch and HC12 off
-								_delay_ms(500);
-								PORTB = 0x00; // all off
-							/*}
-							else if (action == 0x02) {
-								PORTB = 0b00010000; // PB4 ON = reset switch and HC12 off
-								_delay_ms(500);
-								PORTB = 0x00; // all off
-							}
-							else {
-								// bad action nothing to do
-							}
-						}
-						else { 
-							// bad key nothing to do
-						}*/
-						for(int i = 0; i < total_bytes_message; i++) { // reset of message buffer
-							message[i] = 0x00;
-						}
-						
-					}
-					else{
-						// nothing to do
-					}
-				}
-				timeout = 0;
-				PORTB = 0x00; //disable HC12
-				counter++;
-				//go_to_sleep();
-				//go_to_sleep();
+		go_to_sleep();
+		_delay_ms(250); // wait until before new complete message
+		HC12_write(0x01);
+		HC12_read(); 
+		if (action == 0x01) {
+			if (message == key) {
+				PORTB = 0b00001000; // PB3 ON = power switch and HC12 off
+				_delay_ms(500);
+				PORTB = 0x00; // all off
+				_delay_ms(100);
+				HC12_write(0x05);
 			}
-			counter = 0;
-		}
-		else{
-			for(counter=0; counter<=100; counter++){ // enter in sleep for about 100*8 seconds
-				go_to_sleep();
+			else {
+				_delay_ms(100);
+				HC12_write(0x04);
 			}
+		} 	
+		else if (action == 0x02) {
+			if (message == key) {
+				PORTB = 0b00010000; // PB4 ON = reset switch and HC12 off
+				_delay_ms(500);
+				PORTB = 0x00; // all off
+				_delay_ms(100);
+				HC12_write(0x05);
+			}
+			else {
+				_delay_ms(100);
+				HC12_write(0x04);
+			}
+		}	
+		else if (action == 0x03) {
+			read_battery_level();
+			_delay_ms(100);
+			HC12_write(ADCH);
+			HC12_write(ADCL);
+		}	
+		else {
+			_delay_ms(100);
+			HC12_write(0x03);
+		}	
+		for(int i = 0; i < total_bytes_message; i++) { // reset of message buffer
+			message[i] = 0x00;
 		}
     }
 }
